a35c773f866bc53f104119ada2452c37126b4285
ChrisHuntsinger <huntsingerchristian@gmail.com>
2025-07-15 15:33:02 -0500

    split up files

--- stats.py
+++ stats.py
@@ -1,2 +1,10 @@
 def words_in_book(book_words):
    return len(book_words.split())
+
+def chars_in_book():
+    char_dict = {}
+    for char in book_text:
+        if char not in char_dict:
+            char_dict[char] = 1
+        
+            
