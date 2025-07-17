ea57d9fff93d996ea0f61a5bdd17c70d952072ee
ChrisHuntsinger <huntsingerchristian@gmail.com>
2025-07-17 10:18:24 -0500

    Finished, data neatly organized

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 from stats import words_in_book
 from stats import chars_in_book
+from stats import sort_by_char_amount
+import sys
 
 
+if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
-def main(path_to_file):
-    book_text = get_book_text("books/frankenstein.txt")
+def main(path_to_file):    
+    book_text = get_book_text(path_to_file)
     word_amount = words_in_book(book_text)
     amount_of_chars_in_book = chars_in_book(book_text)
+    sorted_chars = sort_by_char_amount(amount_of_chars_in_book)
+
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path_to_file}")
     print("----------- Word Count ----------")
-    print(f"{word_amount} words found in the document")
+    print(f"Found {word_amount} total words")
     print("--------- Character Count -------")
-    print(amount_of_chars_in_book)
+    for item in sorted_chars:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
+    print("============= END ==============")
 
-
-main("books/frankenstein.txt")
+main(sys.argv[1])
--- stats.py
+++ stats.py
@@ -1,15 +1,34 @@
+
+#Returns the amount of words in a book
 def words_in_book(book_words):
    return len(book_words.split())
 
+
+#Calculates different characters in the book, and returns them.
+#Uses a dictionary to do this in the way of ("char": amount_appears_in_book)
 def chars_in_book(book_text):
     char_dict = {}
+
     for char in book_text:
         char = char.lower()
         if char not in char_dict:
             char_dict[char] = 1
         elif char in char_dict:
             char_dict[char] += 1
+
     return char_dict
 
+def sort_on(item):
+    return item["num"]
 
-            
+def sort_by_char_amount(char_dict):
+    sortable_list = []
+
+    for char, num in char_dict.items():
+        sortable_list.append({"char": char, "num": num})
+
+    sortable_list.sort(reverse = True, key = sort_on)
+
+    return sortable_list
+
+    
